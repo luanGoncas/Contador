1190edb28345c558df9ad3993d96dea5be95c453
Luan Santos Gonçalves <luansantos95@hotmail.com>
2022-05-06 01:21:49 -0300

    modificacao para tratar numeros negativos e maiores que 10

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,10 +3,16 @@ var currentNumber = 0;
 
 function increment() {
     currentNumber = currentNumber + 1;
+    if (currentNumber >= 10) {
+        currentNumber = 10;
+    }
     currentNumberWrapper.innerHTML = currentNumber;
 }
 
 function decrement() {
     currentNumber = currentNumber - 1;
+    if (currentNumber < 0) {
+        currentNumber = 0;
+    }
     currentNumberWrapper.innerHTML = currentNumber;
 }
